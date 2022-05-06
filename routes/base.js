9719c6d7e258b2e70a6f37cc506e08288f464eb6
Martin <martin.kaumanns@diebrueder.com>
2022-05-06 16:23:35 +0200

    names shown in partials

--- routes/base.js
+++ routes/base.js
@@ -6,13 +6,16 @@ const routeGuard = require('./../middleware/route-guard');
 const Offer = require('./../models/offer');
 
 router.get('/', (req, res, next) => {
-  Offer.find().sort({ createdAt : -1}).limit(3)
-  .then((offers) => {
-    res.render('home', { offers, title: 'waste mgmt' });
-  })
-  .catch((error) => {
-    next(error)
-  })
+  Offer.find()
+    .sort({ createdAt: -1 })
+    .limit(3)
+    .populate('creator')
+    .then((offers) => {
+      res.render('home', { offers, title: 'waste mgmt' });
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = router;
