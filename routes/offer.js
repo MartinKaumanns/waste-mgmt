d3bbc470ef8558afd04c14a1e130276cff9a9ada
JohannaPeanut <Johanna.k.michel@posteo.de>
2022-05-07 17:04:18 +0200

    preselected checkboxes megamenue route filter

--- routes/offer.js
+++ routes/offer.js
@@ -274,7 +274,84 @@ router.get('/offer-filtered', (req, res, next) => {
     .limit(limit)
     .populate('creator')
     .then((filteredOffers) => {
+      if (!filteredOffers || filteredOffers.length === 0) {
+        res.render('offer-filtered');
+      } else {
+        if(req.query.genres) {
+        if (req.query.genres.includes('Installation')) {
+          filteredOffers[0].installation = true;
+        }
+        if (req.query.genres.includes('Painting')) {
+          filteredOffers[0].painting = true;
+        }
+        if (req.query.genres.includes('Media')) {
+          filteredOffers[0].media = true;
+        }
+        if (req.query.genres.includes('Photography')) {
+          filteredOffers[0].photography = true;
+        }
+        if (req.query.genres.includes('Ceramics')) {
+          filteredOffers[0].ceramics = true;
+        }
+        if (req.query.genres.includes('Performing Arts')) {
+          filteredOffers[0].performingArts = true;
+        }
+        if (req.query.genres.includes('Architecture')) {
+          filteredOffers[0].architecture = true;
+        }
+        if (req.query.genres.includes('Graphics')) {
+          filteredOffers[0].graphics = true;
+        }
+        if (req.query.genres.includes('Other')) {
+          filteredOffers[0].Other = true;
+        }
+      }
+      if(req.query.materials) {
+      if (req.query.materials.includes('wood')) {
+        filteredOffers[0].wood = true;
+      }
+      if (req.query.materials.includes('metal')) {
+        filteredOffers[0].metal = true;
+      }
+      if (req.query.materials.includes('plastic')) {
+        filteredOffers[0].plastic = true;
+      }
+      if (req.query.materials.includes('paper / cardboard')) {
+        filteredOffers[0].paper = true;
+      }
+      if (req.query.materials.includes('pens / brushes')) {
+        filteredOffers[0].pens = true;
+      }
+      if (req.query.materials.includes('paints')) {
+        filteredOffers[0].paints = true;
+      }
+      if (req.query.materials.includes('textile')) {
+        filteredOffers[0].textile = true;
+      }
+      if (req.query.materials.includes('stone / building materials')) {
+        filteredOffers[0].stone = true;
+      }
+      if (req.query.materials.includes('moulding / casting')) {
+        filteredOffers[0].moulding = true;
+      }
+      if (req.query.materials.includes('tools')) {
+        filteredOffers[0].tools = true;
+      }
+      if (req.query.materials.includes('technical equipment')) {
+        filteredOffers[0].technicalEquipment = true;
+      }
+      if (req.query.materials.includes('studio furniture')) {
+        filteredOffers[0].studioFurniture = true;
+      }
+      if (req.query.materials.includes('other')) {
+        filteredOffers[0].other = true;
+      }
+    }
       res.render('offer-filtered', { filteredOffers, queryObj });
+    }
+  })
+    .catch((error) => {
+      next(error)
     });
 });
 
@@ -375,7 +452,6 @@ router.get('/:id/edit', routeGuard, (req, res, next) => {
         if (offer.materials.includes('other')) {
           offer.other = true;
         }
-
         res.render('edit-offer', { offer });
       }
     })
